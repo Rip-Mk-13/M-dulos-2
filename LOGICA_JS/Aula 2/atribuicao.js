6d6126c6c3fbeb172cdeb20bff766c74c99adf8b
Rip-Mk-13 <mateus.augusto.goet@gmail.com>
2025-09-30 22:01:55 -0300

    Javascript_Aula2

---
+++ LOGICA_JS/Aula 2/atribuicao.js
@@ -0,0 +1,45 @@
+function atribuicao1(){
+    var n1 = 55;
+    var n2 = 12;
+
+    console.log(n1 + n2);
+
+    n1 = 10
+    n2 = 15
+    
+    console.log(n1 + n2);
+}
+
+function atribuicao2(){
+    var n1;
+    var n2;
+
+    // var n1 = 55;
+    // var n2 = 12;
+
+    console.log(n1 + n2);
+
+    n1 = 10
+    n2 = 15
+    
+    console.log(n1 + n2);
+}
+
+function atribuicao3(){
+    var n1, n2;
+
+    var n1 = 55;
+    var n2 = 12;
+
+    console.log(n1 + n2);
+
+    n1 = 10
+    n2 = 15
+    
+    console.log(n1 + n2);
+}
+
+atribuicao1();
+atribuicao2();
+atribuicao3();
+
---
+++ LOGICA_JS/Aula 2/codeRun.js
@@ -0,0 +1,21 @@
+// function codeRun(){
+//     console.log("Rodou o code run")
+// }
+
+// codeRun();
+
+function saudacaoBomDia(){
+    console.log("Bom dia!");
+}
+
+function saudacaoBoaTarde(){
+    console.log("Boa tarde!");
+}
+
+function saudacaoBoaNoite(){
+    console.log("Boa noite!");
+}
+
+saudacaoBomDia();
+saudacaoBoaTarde();
+saudacaoBoaNoite();
