147789a555625d5fc78e6929432c336d4278192e
Rip-Mk-13 <mateus.augusto.goet@gmail.com>
2025-10-07 21:51:53 -0300

    atividade aula 2 + Aula 4

---
+++ LOGICA_JS/Aula 2/Dever de Casa/exibindoValoresNoConsole.js
@@ -0,0 +1,11 @@
+function valor(){
+    var exemplo = prompt("Valor inicial")
+    // Decido colocar um valor inicial para acompanhar sua mudança de maneira mais clara
+    console.log(exemplo)
+    // A variável é exibida no console
+
+    exemplo = prompt("Valor Final")
+    // A variavel tem seu valor alterado
+    console.log(exemplo);
+    // O novo valor é exibido no console
+}
